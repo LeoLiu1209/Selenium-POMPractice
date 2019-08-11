33bd1165c4c93ce6d7d48b574546664220d6d243
leoliu <cubemail88@gmail.com>
2019-08-11 20:43:45 +0800

    implement Page Object Model and in order to separting test scripts ans objects

--- tests/login.py
+++ tests/login.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
 import time
 import unittest
+import os, sys
+sys.path.append(os.path.abspath('..'))
+from pages.loginPage import LoginPage
+from pages.homePage import HomePage
+
 
 class LoginTetst(unittest.TestCase):
     @classmethod
@@ -10,12 +15,18 @@ class LoginTetst(unittest.TestCase):
         cls.driver.maximize_window()
 
     def test_Login_valid(self):
-        self.driver.get("https://opensource-demo.orangehrmlive.com")
-        self.driver.find_element_by_id("txtUsername").send_keys("Admin")
-        self.driver.find_element_by_id("txtPassword").send_keys("admin123")
-        self.driver.find_element_by_id("btnLogin").click()
-        self.driver.find_element_by_id("welcome").click()
-        self.driver.find_element_by_link_text("Logout").click()
+        driver = self.driver
+        driver.get("https://opensource-demo.orangehrmlive.com")
+        
+        login = LoginPage(driver)
+        login.enter_username("Admin")
+        login.enter_password("admin123")
+        login.click_login()
+
+        homepage = HomePage(driver)
+        homepage.click_welcome()
+        homepage.click_logout()
+        
         time.sleep(2)
 
     @classmethod
